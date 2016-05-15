fa7b17bd50b00d4e33fd5ec7fe3003216bcc6e84
Marcelo <celogp@gmail.com>
2016-05-15 23:51:29 +0000

    UserModel removido

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,8 @@
 ﻿from django.forms import ModelForm
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 
 #se quiser usar outro form para logar/registrar com o usuário ou complementar o cadastro
-class LoginForm(ModelForm):
-	class Meta:
-		model = User
-		fields = ('username', 'password', 'email')
+#class LoginForm(ModelForm):
+#	class Meta:
+#		model = User
+#		fields = ('username', 'password', 'email')
